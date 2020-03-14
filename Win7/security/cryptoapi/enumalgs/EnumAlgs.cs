43e755a17303366f7b72d1968ec0b96a14206f66
dahall <dahall@byu.net>
2020-03-14 16:57:41 -0600

    Fixed memory pointer bug

--- Win7/security/cryptoapi/enumalgs/EnumAlgs.cs
+++ Win7/security/cryptoapi/enumalgs/EnumAlgs.cs
@@ -15,65 +15,55 @@ namespace enumalgs
 {
 	static class EnumAlgs
 	{
+		[STAThread]
 		static void Main()
 		{
-			SafeHCRYPTPROV hProv;
-			PROV_ENUMALGS EnumAlgs = default;
-			Win32Error dwError = Win32Error.ERROR_SUCCESS;
-
 			// Get handle to the default provider.
-			if (!CryptAcquireContext(out hProv, default, default, CryptProviderType.PROV_RSA_FULL, 0))
+			if (!CryptAcquireContext(out var hProv, default, default, CryptProviderType.PROV_RSA_FULL, 0))
 			{
 				Console.Write("Error {0:X} during CryptAcquireContext!\n", GetLastError());
 				return;
 			}
-
-			// Set the CRYPT_FIRST flag the first time through the loop.
-			uint dwFlags = 1; // CRYPT_FIRST
-
-			// Set size of data expected
-			var dwDataLen = (uint)Marshal.SizeOf(typeof(PROV_ENUMALGS));
-
-			// Enumerate the supported algorithms.
-			using (var pEnumAlgs = new PinnedObject(EnumAlgs))
-			while (dwError.Succeeded)
+			using (hProv)
 			{
-				// Retrieve information about an algorithm.
-				if (!CryptGetProvParam(hProv, ProvParam.PP_ENUMALGS, pEnumAlgs, ref dwDataLen, dwFlags))
+				// Set the CRYPT_FIRST flag the first time through the loop.
+				uint dwFlags = 1; // CRYPT_FIRST
+
+				// Set size of data expected
+				var dwDataLen = (uint)Marshal.SizeOf(typeof(PROV_ENUMALGS));
+
+				// Enumerate the supported algorithms.
+				while (true)
 				{
-					dwError = GetLastError();
-					if (dwError == Win32Error.ERROR_NO_MORE_ITEMS)
+					// Retrieve information about an algorithm.
+					try
 					{
-						// Exit the loop.
+						var EnumAlgs = CryptGetProvParam<PROV_ENUMALGS_EX>(hProv, ProvParam.PP_ENUMALGS_EX, dwFlags);
+
+						// Determine algorithm type.
+						var pszAlgType = (GET_ALG_CLASS(EnumAlgs.aiAlgid)) switch
+						{
+							ALG_CLASS.ALG_CLASS_DATA_ENCRYPT => "Encrypt",
+							ALG_CLASS.ALG_CLASS_HASH => "Hash",
+							ALG_CLASS.ALG_CLASS_KEY_EXCHANGE => "Exchange",
+							ALG_CLASS.ALG_CLASS_SIGNATURE => "Signature",
+							_ => "Unknown",
+						};
+
+						// Print information about algorithm.
+						Console.Write("Name:{0,-19}  Type:{1,-9}  Bits:{2,-4}  Algid:{3}\n",
+							EnumAlgs.szName.ToString(), pszAlgType, EnumAlgs.dwDefaultLen.ToString(), EnumAlgs.aiAlgid);
+
+						// Clear the flags for the remaining interations of the loop.
+						dwFlags = 0;
+					}
+					catch (Win32Exception ex) when (ex.NativeErrorCode == Win32Error.ERROR_NO_MORE_ITEMS)
+					{
+						// This indicates the end of the list.
 						break;
 					}
-
-					Console.Write("Error {0:X} reading algorithm!\n", dwError);
-					goto Error;
 				}
-
-				// Determine algorithm type.
-				var pszAlgType = (GET_ALG_CLASS(EnumAlgs.aiAlgid)) switch
-				{
-					ALG_CLASS.ALG_CLASS_DATA_ENCRYPT => "Encrypt",
-					ALG_CLASS.ALG_CLASS_HASH => "Hash",
-					ALG_CLASS.ALG_CLASS_KEY_EXCHANGE => "Exchange",
-					ALG_CLASS.ALG_CLASS_SIGNATURE => "Signature",
-					_ => "Unknown",
-				};
-
-				// Print information about algorithm.
-				Console.Write("Name:{0,-19}  Type:{1,-9}  Bits:{2,-4}  Algid:{3}\n",
-				   EnumAlgs.szName.ToString(), pszAlgType, EnumAlgs.dwBitLen.ToString(), EnumAlgs.aiAlgid);
-
-				// Clear the flags for the remaining interations of the loop.
-				dwFlags = 0;
 			}
-
-			Error:
-
-			// Release CSP handle (if open)
-			hProv?.Dispose();
 		}
 	}
 }
