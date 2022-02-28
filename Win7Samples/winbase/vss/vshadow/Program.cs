775c54f3a85510938df2bda32af87803fca5e854
dahall <dahall@byu.net>
2022-02-28 11:39:14 -0700

    Added vshadow sample (not working)

---
+++ Win7Samples/winbase/vss/vshadow/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace vshadow
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			VssClient client = new(Vanara.PInvoke.VssApi.VSS_SNAPSHOT_CONTEXT.VSS_CTX_ALL);
+			client.QuerySnapshotSet();
+		}
+	}
+}
---
+++ Win7Samples/winbase/vss/vshadow/Util.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Vanara.Extensions;
+using Vanara.InteropServices;
+using Vanara.PInvoke;
+using static Vanara.PInvoke.Kernel32;
+
+namespace vshadow
+{
+	internal static class Util
+	{
+		// Get the displayable root path for the given volume name
+		public static string GetDisplayNameForVolume(string volumeName)
+		{
+			if (GetVolumePathNamesForVolumeName(volumeName, null, 0, out var dwRequired) || dwRequired <= 2)
+				return "";
+
+			Win32Error.ThrowLastErrorUnless(Win32Error.ERROR_MORE_DATA);
+
+			using var mem = new SafeCoTaskMemHandle(dwRequired);
+			Win32Error.ThrowLastErrorIfFalse(GetVolumePathNamesForVolumeName(volumeName, mem, mem.Size, out dwRequired));
+
+			// compute the smallest mount point by enumerating the returned MULTI_SZ
+			return mem.DangerousGetHandle().ToStringEnum(allocatedBytes: dwRequired).Aggregate((s, best) => s.Length < best.Length ? s : best);
+		}
+	}
+}
