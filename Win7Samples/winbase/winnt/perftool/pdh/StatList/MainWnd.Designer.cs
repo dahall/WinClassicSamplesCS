7d98ac68454527dd1485a02d9a21c1883be26963
dahall <dahall@byu.net>
2022-07-13 14:49:18 -0600

    Added StatList project

---
+++ Win7Samples/winbase/winnt/perftool/pdh/StatList/MainWnd.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace StatList
+{
+	partial class MainWnd
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWnd));
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
+			this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.configureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.addCountersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.clearAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.getDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.helpTopicsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator5 = new System.Windows.Forms.ToolStripSeparator();
+			this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.configureToolStripMenuItem,
+            this.getDataToolStripMenuItem,
+            this.helpToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.toolStripSeparator,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Text = "&File";
+			// 
+			// newToolStripMenuItem
+			// 
+			this.newToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("newToolStripMenuItem.Image")));
+			this.newToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+			this.newToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+			this.newToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+			this.newToolStripMenuItem.Text = "&New";
+			// 
+			// openToolStripMenuItem
+			// 
+			this.openToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("openToolStripMenuItem.Image")));
+			this.openToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+			this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+			this.openToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+			this.openToolStripMenuItem.Text = "&Open";
+			// 
+			// toolStripSeparator
+			// 
+			this.toolStripSeparator.Name = "toolStripSeparator";
+			this.toolStripSeparator.Size = new System.Drawing.Size(143, 6);
+			// 
+			// saveToolStripMenuItem
+			// 
+			this.saveToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("saveToolStripMenuItem.Image")));
+			this.saveToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+			this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+			this.saveToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+			this.saveToolStripMenuItem.Text = "&Save";
+			// 
+			// saveAsToolStripMenuItem
+			// 
+			this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+			this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+			this.saveAsToolStripMenuItem.Text = "Save &As";
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(143, 6);
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+			this.exitToolStripMenuItem.Text = "E&xit";
+			this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+			// 
+			// configureToolStripMenuItem
+			// 
+			this.configureToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addCountersToolStripMenuItem,
+            this.clearAllToolStripMenuItem});
+			this.configureToolStripMenuItem.Name = "configureToolStripMenuItem";
+			this.configureToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+			this.configureToolStripMenuItem.Text = "&Configure";
+			// 
+			// addCountersToolStripMenuItem
+			// 
+			this.addCountersToolStripMenuItem.Name = "addCountersToolStripMenuItem";
+			this.addCountersToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+			this.addCountersToolStripMenuItem.Text = "&Add New Counter...";
+			this.addCountersToolStripMenuItem.Click += new System.EventHandler(this.addCountersToolStripMenuItem_Click);
+			// 
+			// clearAllToolStripMenuItem
+			// 
+			this.clearAllToolStripMenuItem.Name = "clearAllToolStripMenuItem";
+			this.clearAllToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+			this.clearAllToolStripMenuItem.Text = "&Clear All Counters";
+			this.clearAllToolStripMenuItem.Click += new System.EventHandler(this.clearAllToolStripMenuItem_Click);
+			// 
+			// getDataToolStripMenuItem
+			// 
+			this.getDataToolStripMenuItem.Name = "getDataToolStripMenuItem";
+			this.getDataToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F5;
+			this.getDataToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+			this.getDataToolStripMenuItem.Text = "&Get Data!";
+			this.getDataToolStripMenuItem.Click += new System.EventHandler(this.getDataToolStripMenuItem_Click);
+			// 
+			// helpToolStripMenuItem
+			// 
+			this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpTopicsToolStripMenuItem,
+            this.toolStripSeparator5,
+            this.aboutToolStripMenuItem});
+			this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+			this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+			this.helpToolStripMenuItem.Text = "&Help";
+			// 
+			// helpTopicsToolStripMenuItem
+			// 
+			this.helpTopicsToolStripMenuItem.Name = "helpTopicsToolStripMenuItem";
+			this.helpTopicsToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F1;
+			this.helpTopicsToolStripMenuItem.Size = new System.Drawing.Size(220, 22);
+			this.helpTopicsToolStripMenuItem.Text = "&Overview";
+			this.helpTopicsToolStripMenuItem.Click += new System.EventHandler(this.helpTopicsToolStripMenuItem_Click);
+			// 
+			// toolStripSeparator5
+			// 
+			this.toolStripSeparator5.Name = "toolStripSeparator5";
+			this.toolStripSeparator5.Size = new System.Drawing.Size(217, 6);
+			// 
+			// aboutToolStripMenuItem
+			// 
+			this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+			this.aboutToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.OemQuestion)));
+			this.aboutToolStripMenuItem.Size = new System.Drawing.Size(220, 22);
+			this.aboutToolStripMenuItem.Text = "&About...";
+			this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+			// 
+			// MainWnd
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Window;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "MainWnd";
+			this.Text = "Perf Counter Statistics Demo";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator;
+		private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem configureToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem addCountersToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem clearAllToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem getDataToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem helpTopicsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator5;
+		private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+	}
+}
---
+++ Win7Samples/winbase/winnt/perftool/pdh/StatList/MainWnd.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using Vanara.InteropServices;
+using Vanara.PInvoke;
+using static Vanara.PInvoke.Gdi32;
+using static Vanara.PInvoke.Pdh;
+using static Vanara.PInvoke.User32;
+
+namespace StatList;
+
+public partial class MainWnd : Form
+{
+	private const int COUNTER_STRING_SIZE = 1024;
+	private const int NUM_STAT_SAMPLES = 100;
+	private static readonly int[] nTabStops = { 300, 400, 500, 600, 700 };
+	private static readonly string szHelpFileName = System.IO.Path.GetFileNameWithoutExtension(Application.ExecutablePath) + ".hlp";
+
+	// font for text in window
+	private Font hFinePrint = default;
+
+	// PDH Query Handle for these counters
+	private SafePDH_HQUERY hQuery = default;
+
+	// pointer to first item in counter list
+	private readonly List<CIB> pFirstCib = new();
+
+	public MainWnd() => InitializeComponent();
+
+	protected override void OnClosing(CancelEventArgs e)
+	{
+		// Tell WinHelp we don't need it any more...
+		_=WinHelp(Handle, szHelpFileName, HelpCmd.HELP_QUIT, default);
+
+		DeleteAllCounters();
+
+		base.OnClosing(e);
+	}
+
+	protected override void OnLoad(EventArgs e)
+	{
+		base.OnLoad(e);
+
+		if (hQuery is null)
+		{
+			PdhOpenQuery(default, default, out hQuery).ThrowIfFailed();
+		}
+		if (hFinePrint is null)
+		{
+			hFinePrint = new Font(Font.Name, 8f);
+		}
+	}
+
+	protected override void OnMouseDown(MouseEventArgs e)
+	{
+		base.OnMouseDown(e);
+
+		if (e.Button != MouseButtons.Right)
+			return;
+
+		// This is where you would determine the appropriate 'context' menu to bring up. Since this app has no real functionality, we will
+		// just bring up the 'configure' menu:
+		var ctx = new ContextMenuStrip();
+		foreach (ToolStripMenuItem mi in configureToolStripMenuItem.DropDown.Items)
+			_=ctx.Items.Add(mi);
+		ctx.Show(this, e.Location);
+	}
+
+	protected override void OnPaint(PaintEventArgs e)
+	{
+		base.OnPaint(e);
+
+		int nX = 0, nY = menuStrip1.Height;
+
+		// draw Title text
+		using var hdc = new SafeTempHDC(e.Graphics);
+		var szOutputString = "Performance Counter\tLast Value\tMinimum\tMaximum\tAverage";
+		var lTextOutReturn = TabbedTextOut(hdc, nX, nY, szOutputString, szOutputString.Length, nTabStops.Length, nTabStops, 0);
+		nY += Macros.HIWORD(unchecked((uint)lTextOutReturn));
+
+		// select the fine print font for this window
+		using var hfont = new SafeHFONT(hFinePrint.ToHfont());
+		_=SelectObject(hdc, hfont);
+
+		// for each CIB in the list draw the current text and value
+		foreach (CIB pCib in pFirstCib)
+		{
+			szOutputString = $"{pCib.szCounterPath}\t{pCib.dLastValue}\t{pCib.pdhCurrentStats.min.doubleValue}\t{pCib.pdhCurrentStats.max.doubleValue}\t{pCib.pdhCurrentStats.mean.doubleValue}";
+			lTextOutReturn = TabbedTextOut(hdc, nX, nY, szOutputString, szOutputString.Length, nTabStops.Length, nTabStops, 0);
+			nY += Macros.HIWORD(unchecked((uint)lTextOutReturn));
+		}
+	}
+
+	private void aboutToolStripMenuItem_Click(object sender, EventArgs e) => new AboutDlg().ShowDialog(this);
+
+	private void addCountersToolStripMenuItem_Click(object sender, EventArgs e)
+	{
+		SafeCoTaskMemString szCounterBuffer = new(COUNTER_STRING_SIZE);
+		PDH_BROWSE_DLG_CONFIG BrowseInfo = new()
+		{
+			Flags = BrowseFlag.bSingleCounterPerDialog | BrowseFlag.bSingleCounterPerAdd | BrowseFlag.bIncludeCostlyObjects,
+			hWndOwner = Handle,
+			szReturnPathBuffer = szCounterBuffer,
+			cchReturnPathLength = COUNTER_STRING_SIZE,
+			CallBackStatus = Win32Error.ERROR_SUCCESS,
+			dwDefaultDetailLevel = PERF_DETAIL.PERF_DETAIL_WIZARD,
+			szDialogBoxCaption = "Select a counter to monitor"
+		};
+
+		if (PdhBrowseCounters(ref BrowseInfo).Succeeded)
+		{
+			// try to add the counter to the query
+			if (PdhAddCounter(hQuery, szCounterBuffer, default, out SafePDH_HCOUNTER hCounter).Succeeded)
+			{
+				// add counter to the list
+				CIB pNewCib = new()
+				{
+					hCounter = hCounter,
+					szCounterPath = szCounterBuffer,
+					// allocate the raw data buffer here
+					pCounterArray = new PDH_RAW_COUNTER[NUM_STAT_SAMPLES]
+				};
+				pNewCib.pdhCurrentStats.min.CStatus = Win32Error.PDH_CSTATUS_INVALID_DATA;
+				pNewCib.pdhCurrentStats.max.CStatus = Win32Error.PDH_CSTATUS_INVALID_DATA;
+				pNewCib.pdhCurrentStats.mean.CStatus = Win32Error.PDH_CSTATUS_INVALID_DATA;
+
+				//add to the top of the list
+				pFirstCib.Add(pNewCib);
+
+				// repaint window to get new entry
+				Refresh();
+			} // else unable to add counter
+		} // else user cancelled
+	}
+
+	private void clearAllToolStripMenuItem_Click(object sender, EventArgs e)
+	{
+		// delete all current counters, then create a new query
+		DeleteAllCounters();
+
+		_=PdhOpenQuery(default, default, out hQuery);
+
+		Refresh();
+	}
+
+	private void DeleteAllCounters()
+	{
+		// close PDH Query this removes all counters from the query as well as removes the query item itself
+		if (hQuery is not null)
+		{
+			hQuery.Dispose();
+			hQuery = default;
+		}
+
+		// clean up any memory allocations
+		pFirstCib.Clear();
+	}
+
+	private void exitToolStripMenuItem_Click(object sender, EventArgs e) => Close();
+
+	private void getDataToolStripMenuItem_Click(object sender, EventArgs e)
+	{
+		if (hQuery is null)
+			return;
+
+		// get the current values of the query data
+		if (PdhCollectQueryData(hQuery).Succeeded)
+		{
+			// loop through all counters and update the display values and statistics
+			foreach (CIB pCib in pFirstCib)
+			{
+				// update "Last value"
+				_=PdhGetFormattedCounterValue(pCib.hCounter, PDH_FMT.PDH_FMT_DOUBLE, out _, out PDH_FMT_COUNTERVALUE pValue);
+				pCib.dLastValue = pValue.doubleValue;
+				// update "Raw Value" and statistics
+				_=PdhGetRawCounterValue(pCib.hCounter, out _, out PDH_RAW_COUNTER pRaw);
+				pCib.pCounterArray[pCib.dwNextIndex] = pRaw;
+				_=PdhComputeCounterStatistics(pCib.hCounter, PDH_FMT.PDH_FMT_DOUBLE, pCib.dwFirstIndex, ++pCib.dwLastIndex, pCib.pCounterArray, out pCib.pdhCurrentStats);
+				// update pointers & indeces
+				if (pCib.dwLastIndex < NUM_STAT_SAMPLES)
+				{
+					pCib.dwNextIndex = ++pCib.dwNextIndex % NUM_STAT_SAMPLES;
+				}
+				else
+				{
+					--pCib.dwLastIndex;
+					pCib.dwNextIndex = pCib.dwFirstIndex;
+					pCib.dwFirstIndex = ++pCib.dwFirstIndex % NUM_STAT_SAMPLES;
+				}
+			}
+			// cause the window to be repainted with the new values (NOTE: This isn't the most efficient method of display updating.)
+			Refresh();
+		}
+	}
+
+	private void helpTopicsToolStripMenuItem_Click(object sender, EventArgs e)
+	{
+		var bGotHelp = WinHelp(Handle, szHelpFileName, HelpCmd.HELP_FINDER, default);
+		if (!bGotHelp)
+		{
+			_=System.Windows.Forms.MessageBox.Show(this, "Unable to activate help", null, MessageBoxButtons.OK, MessageBoxIcon.Hand);
+		}
+	}
+
+	private class CIB
+	{
+		public double dLastValue;
+		public uint dwFirstIndex;
+		public uint dwLastIndex;
+		public uint dwNextIndex;
+		public SafePDH_HCOUNTER hCounter;
+		public PDH_RAW_COUNTER[] pCounterArray;
+		public PDH_STATISTICS pdhCurrentStats;
+		public string szCounterPath;
+	}
+}
