da0d55f1be6e1c5d115fb3dfc82d4cf65b1d5ba7
dahall <dahall@byu.net>
2022-07-25 15:27:40 -0600

    Added BITS samples CONTROLS and DOWNLOADS

---
+++ Win7Samples/web/bits/CONTROLS/CONTROLS.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using Vanara.IO;
+
+if (args.Length != 2)
+{
+	Console.Write("Usage: CONTROLS.exe [remote name] [local name]\n");
+	return;
+}
+
+try
+{
+	// Create a Job
+	BackgroundCopyJob pJob = BackgroundCopyManager.Jobs.Add("MyJobName", "", BackgroundCopyJobType.Download);
+
+	// Add a File
+	pJob.Files.Add(args[0], args[1]);
+
+	// Enable local caching for this job
+	BackgroundCopyManager.PeerCacheAdministration.ConfigurationFlags = PeerCaching.Enable | PeerCaching.EnableServer;
+
+	// Set Max Cache Size to 2% of disk
+	BackgroundCopyManager.PeerCacheAdministration.MaximumCacheSize = 2;
+
+	//Resume the job
+	AutoResetEvent evt = new(false);
+	pJob.Completed += (s, e) => evt.Set();
+	pJob.Error += (s, e) => evt.Set();
+	pJob.Resume();
+
+	//Wait for completion
+	evt.WaitOne(TimeSpan.FromMinutes(5));
+
+	if (pJob.State == BackgroundCopyJobState.Transferred)
+	{
+		pJob.Complete();
+	}
+	else
+	{
+		pJob.Cancel();
+	}
+}
+catch (Exception ex)
+{
+	Console.WriteLine($"Failure at {new System.Diagnostics.StackTrace(ex).GetFrame(0).GetMethod().Name}: {ex.Message}");
+}
---
+++ Win7Samples/web/bits/DOWNLOADS/DOWNLOADS.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using Vanara.IO;
+
+if (args.Length != 2)
+{
+Console.Write("Usage: DOWNLOADS.exe [remote name] [local name]\n");
+	return;
+}
+
+try
+{
+	// Create a Job
+	Console.Write("Creating Job...\n");
+	var pJob = BackgroundCopyManager.Jobs.Add("P2PSample", "", BackgroundCopyJobType.Download);
+
+	// Set the File Completed Call
+	var evt = new AutoResetEvent(false);
+	pJob.Completed += (s, e) => {
+		Console.WriteLine("Job transferred. Completing Job...");
+		try { pJob.Complete(); evt.Set(); }
+		catch (Exception jcex) { Console.WriteLine($"Job Completion Failed with error {jcex.Message}"); }
+	};
+	pJob.Error += (s, e) => {
+		Console.WriteLine("Job entered error state...");
+		Console.WriteLine($"Job {e.Job.DisplayName} encountered the following error: {e.Job.LastError}");
+		evt.Set();
+	};
+	// Add a File
+	Console.Write("Adding File to Job\n");
+	pJob.Files.Add(args[0], args[1]);
+
+	//Resume the job
+	Console.Write("Resuming Job...\n");
+	pJob.Resume();
+
+	// Wait for QuitMessage from CallBack
+	evt.WaitOne(TimeSpan.FromMinutes(15));
+}
+catch (Exception ex)
+{
+	Console.WriteLine($"Failure at {new System.Diagnostics.StackTrace(ex).GetFrame(0).GetMethod().Name}: {ex.Message}");
+}
