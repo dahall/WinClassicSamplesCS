1303cc35fdbfae3ac5b06b519962e7ce2b8fa088
David Hall <dahall@byu.net>
2024-06-14 19:45:41 -0400

    Added UsingImageFactory project

---
+++ Win7Samples/winui/shell/appplatform/UsingImageFactory/ImageFactoryDlg.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ImageFactorySample;
+
+partial class ImageFactoryDlg
+{
+	/// <summary>
+	/// Required designer variable.
+	/// </summary>
+	private System.ComponentModel.IContainer components = null;
+
+	/// <summary>
+	/// Clean up any resources being used.
+	/// </summary>
+	/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+	protected override void Dispose(bool disposing)
+	{
+		if (disposing && (components != null))
+		{
+			components.Dispose();
+		}
+		base.Dispose(disposing);
+	}
+
+	#region Windows Form Designer generated code
+
+	/// <summary>
+	/// Required method for Designer support - do not modify
+	/// the contents of this method with the code editor.
+	/// </summary>
+	private void InitializeComponent()
+	{
+			this.IDCANCEL = new System.Windows.Forms.Button();
+			this.IDOK = new System.Windows.Forms.Button();
+			this.IDC_STATIC1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.IDC_STATIC1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// IDCANCEL
+			// 
+			this.IDCANCEL.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.IDCANCEL.Location = new System.Drawing.Point(209, 162);
+			this.IDCANCEL.Name = "IDCANCEL";
+			this.IDCANCEL.Size = new System.Drawing.Size(75, 23);
+			this.IDCANCEL.TabIndex = 0;
+			this.IDCANCEL.Text = "Cancel";
+			this.IDCANCEL.UseVisualStyleBackColor = true;
+			// 
+			// IDOK
+			// 
+			this.IDOK.Location = new System.Drawing.Point(128, 162);
+			this.IDOK.Name = "IDOK";
+			this.IDOK.Size = new System.Drawing.Size(75, 23);
+			this.IDOK.TabIndex = 0;
+			this.IDOK.Text = "OK";
+			this.IDOK.UseVisualStyleBackColor = true;
+			// 
+			// IDC_STATIC1
+			// 
+			this.IDC_STATIC1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.IDC_STATIC1.Location = new System.Drawing.Point(13, 13);
+			this.IDC_STATIC1.Name = "IDC_STATIC1";
+			this.IDC_STATIC1.Size = new System.Drawing.Size(271, 143);
+			this.IDC_STATIC1.TabIndex = 1;
+			this.IDC_STATIC1.TabStop = false;
+			// 
+			// ImageFactoryDlg
+			// 
+			this.AcceptButton = this.IDOK;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CancelButton = this.IDCANCEL;
+			this.ClientSize = new System.Drawing.Size(296, 197);
+			this.Controls.Add(this.IDC_STATIC1);
+			this.Controls.Add(this.IDOK);
+			this.Controls.Add(this.IDCANCEL);
+			this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.Name = "ImageFactoryDlg";
+			this.Text = "IShellItemImageFactory Sample";
+			((System.ComponentModel.ISupportInitialize)(this.IDC_STATIC1)).EndInit();
+			this.ResumeLayout(false);
+
+	}
+
+	#endregion
+
+	private System.Windows.Forms.Button IDCANCEL;
+	private System.Windows.Forms.Button IDOK;
+	public System.Windows.Forms.PictureBox IDC_STATIC1;
+}
---
+++ Win7Samples/winui/shell/appplatform/UsingImageFactory/ImageFactorySample.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Vanara.PInvoke;
+using static Vanara.PInvoke.Shell32;
+
+namespace ImageFactorySample;
+
+internal class Program
+{
+	// The possbile sizes for the image that is requested
+	private static readonly Dictionary<string, int> sizeTable = new() { { "small", 16 }, { "medium", 48 }, { "large", 96 }, { "extralarge", 256 } };
+
+	private static int LookUp(string pwszArg) => sizeTable.TryGetValue(pwszArg, out int val) ? val : 0;
+
+	private static void DisplayUsage()
+	{
+		Console.Write("Usage:\n");
+		Console.Write("IShellItemImageFactory.exe <size> <Absolute Path to file>\n");
+		Console.Write("size - small, medium, large, extralarge\n");
+		Console.Write("e.g. ImageFactorySample.exe medium c:\\HelloWorld.jpg \n");
+	}
+
+	[STAThread]
+	private static void Main(string[] args)
+	{
+		if (args.Length != 2)
+		{
+			DisplayUsage();
+		}
+		else
+		{
+			int nSize = LookUp(args[0]);
+			if (nSize == 0)
+			{
+				DisplayUsage();
+			}
+			else
+			{
+				try
+				{
+					// Getting the IShellItemImageFactory interface pointer for the file.
+					IShellItemImageFactory pImageFactory = SHCreateItemFromParsingName<IShellItemImageFactory>(args[1])!;
+					SIZE size = new(nSize, nSize);
+
+					//sz - Size of the image, SIIGBF_BIGGERSIZEOK - GetImage will stretch down the bitmap (preserving aspect ratio)
+					pImageFactory.GetImage(size, SIIGBF.SIIGBF_BIGGERSIZEOK, out var hbmp).ThrowIfFailed();
+					var dlg = new ImageFactoryDlg();
+					dlg.IDC_STATIC1.Image = hbmp.ToBitmap();
+					dlg.ShowDialog();
+				}
+				catch (Exception ex)
+				{
+					System.Windows.Forms.MessageBox.Show(ex.Message, "Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
+				}
+			}
+		}
+	}
+}
